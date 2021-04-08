2c65d76bd4f0790277741cf09745e18a5159fc14
PPapple149 <parvathirpai@outlook.com>
2021-04-08 10:46:17 -0700

    Razor pages

---
+++ BookList/BookListRazor/BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using BookListRazor.model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+
+        private readonly ApplicationDBContext _db;
+        public BookController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Book.ToList() });
+        }
+    }
+}
